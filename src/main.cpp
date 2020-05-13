5ca2424f6cfce876abf660e883d8b17fe8f15e0a
Tobias Jensen <tobias@Terra>
2020-05-13 22:59:20 +0200

    Just a main that tests functionality and speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <chrono>
 
 #include "program.hpp"
-
-extern void yyparser();
+#include "requester.hpp"
+#include "jsonParser.hpp"
 
 int main()
 {
+    Requester req;
+
+    req.setHost("https://www.reddit.com/");
+    std::string t = req.getResponse("/.json");
+
+    JsonParser parser;
+
+    auto before = std::chrono::high_resolution_clock::now();
+    parser.parse(t);
+    auto now = std::chrono::high_resolution_clock::now();
+    
+    auto dT = std::chrono::duration_cast<std::chrono::microseconds>(now - before).count();
+
+    std::cout << dT << std::endl;
+
     return 0;
 }
